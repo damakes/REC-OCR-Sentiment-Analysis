28e8f4445b658976c8ae5546e8597eef031eafdb
Markus Fohr <155246347+damakes@users.noreply.github.com>
2024-04-17 20:51:36 +0300

    Update REC_OCR.py

--- REC/REC_OCR.py
+++ REC/REC_OCR.py
@@ -4,76 +4,33 @@ from PIL import ImageGrab
 import time
 import pytesseract
 from textblob import TextBlob
-#https://github.com/UB-Mannheim/tesseract/wiki
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe' # Path to the Tesseract.exe
 
-def capture_screen():
-    """
-    Capture the screen within the defined bounding box.
-    Returns:
-        np.array: The captured screen screen as a NumPy array.
-    """
+def capture_screen(): #Returns: The captured screen screen as a NumPy array.
     return np.array(ImageGrab.grab(bbox=(0, 40, 1200, 800)))
 
-def convert_to_grayscale(screen):
-    """
-    Convert the given screen to grayscale.
-    Args:
-        screen (np.array): The input screen as a NumPy array.
-    Returns:
-        np.array: The grayscale version of the input screen.
-    """
+def convert_to_grayscale(screen): # Returns: The grayscale version of the input screen.
     return cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
 
-def recognize_text(screen):
-    """
-    Use OCR to recognize text from the given screen.
-    Args:
-        screen (np.array): The input screen as a NumPy array.
-    Returns:
-        str: The recognized text.
-    """
+def recognize_text(screen): # Returns: The recognized text.
     return pytesseract.image_to_string(screen)
 
-def write_text_to_file(text, file_path):
-    """
-    Write the given text to the specified file.
-    Args:
-        text (str): The text to write to the file.
-        file_path (str): The path to the output file.
-    """
+def write_text_to_file(text, file_path): # Write the given text to the specified file.
     with open(file_path, "a") as output_file: output_file.write(text + "\n")
 
-def draw_rectangles(screen, boxes):
-    """
-    Draw rectangles around recognized words on the given screen.
-    Args:
-        screen (np.array): The input screen as a NumPy array.
-        boxes (str): The bounding boxes of recognized words.
-    Returns:
-        np.array: The screen with rectangles drawn around recognized words.
-    """
+def draw_rectangles(screen, boxes): # Returns: np.array: The screen with rectangles drawn around recognized words.
     h, w, _ = screen.shape # Get the height and width of the screen image
     for b in boxes.splitlines(): # Iterate over each line of bounding box information
         b = b.split() # Split the line into individual values
         x, y, x2, y2 = int(b[1]), int(b[2]), int(b[3]), int(b[4]) # Extract and convert bounding box coordinates from the Tesseract OCR result
         cv2.rectangle(screen, (x, h - y), (x2, h - y2), (0, 255, 0), 2) # Draw rectangle around the word
         cv2.putText(screen, b[0], (x, h - y), cv2.FONT_HERSHEY_COMPLEX, 0.65, (255, 0, 0), 2) # Add text label to the rectangle
-    return screen
+    return screen 
 
 def analyze_sentiment(file_path):
-    """
-    Read the text from the specified file and return its polarity.
-
-    Args:
-        file_path (str): The path to the file.
-
-    Returns:
-        str: The polarity of the text ('positive', 'negative', or 'neutral').
-    """
-    # Read the text from the file
-    with open(file_path, 'r') as file: text = file.read()
+    
+    with open(file_path, 'r') as file: text = file.read() # Read the text from the file
 
     blob = TextBlob(text)
     polarity = blob.sentiment.polarity
@@ -85,10 +42,8 @@ def analyze_sentiment(file_path):
 
 
 
-def main():
-    """
-    Main function to capture screen, recognize text, and display results.
-    """
+def main(): # Main function to capture screen, recognize text, and display results.
+ 
     last_time = time.time()
     file_path = "recognized_text.txt"
     while True:
